84fcc8f74acbc3271a0cb74c06d80cdf7c5fa8af
boanerges <boanergesrequena@fasddafa.com>
2024-01-19 11:16:26 -0600

    mi primer repositorio

---
+++ src/ejemplo4/Ejemplo4.java
@@ -0,0 +1,26 @@
+
+package ejemplo4;
+
+
+public class Ejemplo4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Boanerges");
+         System.out.println("Joel");
+          System.out.println("de Jesus");
+           System.out.println("Requena");
+            System.out.println("Vaquerano");
+             System.out.println("17 años");
+              System.out.println("50123842");
+               System.out.println("kaminal juyu 2");
+                System.out.println("5to diver");
+                 System.out.println("hilachas");
+                  System.out.println("masculino");
+                   System.out.println("futbol");
+                    
+    }
+    
+}
